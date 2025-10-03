288d4af21b82b3d2ebcbfb587cb878758dc6a87e
irmasmajlovic <156287214+irmasmajlovic@users.noreply.github.com>
2025-10-02 19:39:10 -0600

    Update adder.v

--- adder.v
+++ adder.v
@@ -1,9 +1,11 @@
 module adder(
-    // Declare your A/B inputs
-    // Declare Y output
-    // Declare carry output
+    input A,
+    input B,
+   
+    output Y,
+    
+    output C
 );
-
-    // Enter logic equation here
-
+    assign Y = A ^ B;
+    assign C = A & B;
 endmodule
